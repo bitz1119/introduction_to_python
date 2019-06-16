be66cc5b08ed57e22cb4fb1282c68177c5c6dd06
bittoo aggarwal <bitz1119@gmail.com>
2019-06-16 12:05:52 +0530

    class 4 classes modules

---
+++ class-4/abc.py
@@ -0,0 +1,15 @@
+import sys
+if(sys.argv[1] =="bittoo-aggarwal"):
+	import module1
+	print(sys.path)
+	print(sys.argv)
+	module1.printnstar(10)
+else:
+	print("wrong password") 
+
+
+
+
+
+
+
---
+++ class-4/module1.py
@@ -0,0 +1,2 @@
+def printnstar(n):
+	print('*'*n)
